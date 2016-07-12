0659fcfa58d6877c783f1a74466ee1bba4b44bc3
dantesrevelion <dantesrevelion@gmail.com>
2016-07-12 18:58:11 -0500

    agregando toolbar a menu

--- MiPedido/MiPedido/app/src/main/java/com/example/dantesrevelion/mipedido/Menu.java
+++ MiPedido/MiPedido/app/src/main/java/com/example/dantesrevelion/mipedido/Menu.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class Menu extends AppCompatActivity {
   //  JSONArray jsonArray;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
         final ListView lista = (ListView) findViewById(R.id.listaMenu);
         final String[] items ={"Productos","Mi Perfil","Reporte por Fecha","Lista de Ventas","Registro de Tickets"};
         VysorAdapterMenu adapter = new VysorAdapterMenu(Menu.this, R.layout.item_list, items);
@@ -52,6 +57,18 @@ public class Menu extends AppCompatActivity {
     @Override
     public  void onBackPressed(){
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.mipedido_menu, menu);
+        //return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
     }
     /*
     private class Connection extends AsyncTask {
